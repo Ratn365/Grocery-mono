88dbee24db70f5ae095529de40bbb6b8b588cbb8
Ratn365 <Ratnesh.visionias@outlook.com>
2024-06-27 15:26:14 +0530

    manage server error

--- start/src/app.js
+++ start/src/app.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  // throw new Error("error");
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app
+  .listen(port, () => {
+    console.log(` app listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.log(err);
+    process.exit();
+  });
